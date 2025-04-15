de3271cbdc42b87e37c30ae1111027823a3d48ca
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-15 17:48:24 +0200

    Add reviews retrieval to movie details in MoviesController

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -17,11 +17,22 @@ function show(req, res) {
     const id = Number(req.params.id)
 
     const sql = 'SELECT * FROM movies WHERE id = ?'
+    const sqlReviews = 'SELECT * FROM reviews WHERE movie_id = ?'
+
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: err.message })
         if (results.length === 0) return res.status(404).json({ error: 'Film non trovato' })
         const movie = results[0]
-        res.json(movie)
+
+        connection.query(sqlReviews, [id], (err, reviews) => {
+            if (err) return res.status(500).json({ error: err.message })
+            movie.reviews = reviews
+            console.log(movie.reviews);
+
+            res.json(movie)
+
+        })
+
     })
 }
 
