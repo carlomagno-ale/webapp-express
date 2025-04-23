b382761c8e97a5d54b67e852e66d8ca7993b3594
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-23 17:07:56 +0200

    Remove storeReview function from MoviesController and update routes to use ReviewController

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -34,26 +34,7 @@ function show(req, res) {
     })
 }
 
-/*// store a movie review
-function storeReview(req, res) {
-
-    const id = Number(req.params.id)
-
-    const { name, vote, text } = req.body
-
-    const insertSql = 'INSERT INTO reviews (movie_id, name, vote, text) VALUES (?, ?, ?, ?)'
-    const values = [id, name, vote, text]
-
-    connection.query(insertSql, values, (err, results) => {
-        if (err) return res.status(500).json({ error: err.message })
-        console.log(results)
-        res.status(201).json({ message: 'Recensione aggiunta', reviewId: results.insertId })
-    })
-
-}*/
-
 module.exports = {
     index,
     show,
-    //storeReview,
 }
--- routes/movies.js
+++ routes/movies.js
@@ -9,7 +9,6 @@ router.get('/', MoviesController.index)
 router.get('/:id', MoviesController.show)
 
 //route per recensione
-//router.post('/:id/review', MoviesController.storeReview)
 router.post('/:id/review', ReviewController.storeReview)
 
 module.exports = router;
